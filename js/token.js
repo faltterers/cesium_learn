3603e2122a849220c1da3fd1c1a0e4fdbbe4e7aa
faltterers <71960204+faltterers@users.noreply.github.com>
2023-03-21 10:11:15 +0800

    Update token.js

--- js/token.js
+++ js/token.js
@@ -6,7 +6,7 @@
  * @LastEditors: flatterer
  * @LastEditTime: 2023-03-21 09:34:42
  */
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJmMzkwYTNlNy1hNDYwLTQ1NGQtYTlhNi0xNzEwNjEzMDhjMGMiLCJpZCI6MTI5NDQ0LCJpYXQiOjE2NzkyOTQ3NzB9.n0KRQHhjyoT1q0w39RI66iV6KoPjshFuTUJQDkxivmk';
+Cesium.Ion.defaultAccessToken = '此处替换为自己申请的token';
 viewer = new Cesium.Viewer("cesiumContainer",{
     terrainProvider: Cesium.createWorldTerrain()
 });
